4d1f87fe91fbc69da34f1b7a74c900b2adc9283d
ivo-n-g <ganzaivo1@gmail.com>
2026-04-22 11:17:00 +0200

    feat: integrate official Google OAuth and functional email/password auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { LanguageProvider } from './context/LanguageContext'
 import { ThemeProvider } from './context/ThemeContext'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './index.css'
 
+// Replace with your actual Client ID from Google Cloud Console
+const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <ThemeProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )

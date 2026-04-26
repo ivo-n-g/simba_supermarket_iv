19009ff81a5d343baae09dea2e8d5319de5ea0b1
ivo-n-g <ganzaivo1@gmail.com>
2026-04-26 17:46:39 +0200

    style: ensure static search page until Enter is hit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ onSearch, onLogoClick, onOpenBranchDash
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInputValue(value);
-    onSearch(value);
+    // onSearch(value); // Removed live search to ensure static page until Enter is hit
     setShowSuggestions(true);
   };
 
@@ -92,9 +92,13 @@ const Header: React.FC<HeaderProps> = ({ onSearch, onLogoClick, onOpenBranchDash
 
   const handleSuggestionClick = (query: string) => {
     setInputValue(query);
-    onSearch(query);
-    addToHistory(query);
-    setShowSuggestions(false);
+    setIsAiLoading(true);
+    setTimeout(() => {
+      onSearch(query);
+      addToHistory(query);
+      setShowSuggestions(false);
+      setIsAiLoading(false);
+    }, 300);
   };
 
   const handleAuthClick = (e: React.MouseEvent) => {
